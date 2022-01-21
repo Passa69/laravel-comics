3ff54bfc89076a3b60c417277372e9b2d19cc74d
tommaso <passarotto13@gmail.com>
2022-01-21 18:07:50 +0100

    creazione sezione di action

--- MyLaravelComics/resources/views/pages/action.blade.php
+++ MyLaravelComics/resources/views/pages/action.blade.php
@@ -1,4 +1,50 @@
 @extends('layouts.main-layout')
 @section('content')
-    <h1>Hello</h1>
+    <div id="action_up">
+
+    </div>
+
+    <section id="grey">
+        <div id="action_down">
+            <div>
+                <div class="action">
+                    <h2>Talent</h2>
+                    <div>
+                        <span class="action_title">Art by:</span>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At, saepe? Sit atque ratione, officiis, sint asperiores modi illo sed velit id quas amet in suscipit autem commodi quod magnam! Recusandae!</p>
+                    </div>
+                    <div>
+                        <span class="action_title">Written by:</span>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At, saepe? Sit atque ratione, officiis, sint asperiores modi illo sed velit id quas amet in suscipit autem commodi quod magnam! Recusandae!</p>
+                    </div>
+                </div>
+    
+                <div class="action">
+                    <h2>Specs</h2>
+                    <div>
+                        <span class="action_title">Series:</span>
+                        <span>ACTION COMICS</span>
+                    </div>
+                    <div>
+                        <span class="action_title">Us price:</span>
+                        <span>19.99$</span>
+                    </div>
+                    <div>
+                        <span class="action_title">On Sale date:</span>
+                        <span>Oct 02 2018</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <nav id="action_nav">
+            <ul>
+                <li><a href="#">DIGITAL COMICS</a></li>
+                <li><a href="#">DC MERCHANDISE</a></li>
+                <li><a href="#">SUBSCRIPTION</a></li>
+                <li><a href="#">COMIC SHOP LOCATOR</a></li>
+                <li><a href="#">DC POWER VISA</a></li>
+            </ul>
+        </nav>
+    </section>
+    
 @endsection
