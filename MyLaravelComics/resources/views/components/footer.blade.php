8e9dd742fb52fd4147392c9691e83652619aeb34
tommaso <passarotto13@gmail.com>
2022-01-21 16:42:14 +0100

    conclusione css footer

--- MyLaravelComics/resources/views/components/footer.blade.php
+++ MyLaravelComics/resources/views/components/footer.blade.php
@@ -1,5 +1,45 @@
 <footer>
     <div id="foot_up">
+        <div>
+            <ul>
+                <li class="list_title">DC COMICS</li>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Videos</a></li>
+            </ul>
+            <ul>
+                <li class="list_title">SHOP</li>
+                <li><a href="#">shop DC</a></li>
+                <li><a href="#">shop DC Colletibles</a> </li>
+            </ul>
+        </div>
+
+        <ul>
+            <li class="list_title">DC</li>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">Videos</a></li>
+        </ul>
+
+        <ul>
+            <li class="list_title">SITES</li>
+            <li><a href="#">shop DC</a></li>
+            <li><a href="#">shop DC Colletibles</a> </li>
+            <li><a href="#">shop DC</a></li>
+            <li><a href="#">shop DC Colletibles</a> </li>
+        </ul>
         
     </div>
 
