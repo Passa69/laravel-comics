4a275a0103d761f662e04ed95c3a9274cb28ed7a
tommaso <passarotto13@gmail.com>
2022-01-21 16:27:51 +0100

    costruzione css header e inizio footer

--- MyLaravelComics/resources/views/components/footer.blade.php
+++ MyLaravelComics/resources/views/components/footer.blade.php
@@ -1,3 +1,13 @@
 <footer>
-    <h2>hello foot</h2>
+    <div id="foot_up">
+        
+    </div>
+
+    <div id="foot_down">
+        <button>SIGN-UP NOW!</button>
+
+        <div>
+            <h5>FOLLOW US</h5>
+        </div>
+    </div>
 </footer>
--- MyLaravelComics/resources/views/components/header.blade.php
+++ MyLaravelComics/resources/views/components/header.blade.php
@@ -1,3 +1,22 @@
 <header>
-    <h2>hello head</h2>
+    <nav>
+        <h2>DC</h2>
+
+        <ul>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#">COMICS</a></li>
+            <li><a href="#">MOVIES</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">GAMES</a></li>
+            <li><a href="#">COLLECTIBLE</a></li>
+            <li><a href="#">VIDEOS</a></li>
+            <li><a href="#">FANS</a></li>
+            <li><a href="#">NEWS</a></li>
+            <li><a href="#">SHOP</a></li>
+        </ul>
+    </nav>
+    
+    <div id="jumbo">
+        
+    </div>
 </header>
