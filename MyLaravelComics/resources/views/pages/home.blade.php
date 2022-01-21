b3763b79dcd4dc67d17cf3f1be29ff29d7d98140
tommaso <passarotto13@gmail.com>
2022-01-21 16:59:46 +0100

    creazione home

--- MyLaravelComics/resources/views/pages/home.blade.php
+++ MyLaravelComics/resources/views/pages/home.blade.php
@@ -1,16 +1,54 @@
 @extends('layouts.main-layout')
 @section('content')
-    <div>
+    <section id="home_sec">
+        <div id="comics">
+            <div class="comic">
+                <h5><a href="">Action comics</a></h5>
+            </div>
+            <div class="comic">
+                <h5><a href="">Action comics</a></h5>
+            </div>
+            <div class="comic">
+                <h5><a href="">Action comics</a></h5>
+            </div>
+            <div class="comic">
+                <h5><a href="">Action comics</a></h5>
+            </div>
+            <div class="comic">
+                <h5><a href="">Action comics</a></h5>
+            </div>
+            <div class="comic">
+                <h5><a href="">Action comics</a></h5>
+            </div>
+            <div class="comic">
+                <h5><a href="">Action comics</a></h5>
+            </div>
+            <div class="comic">
+                <h5><a href="">Action comics</a></h5>
+            </div>
+            <div class="comic">
+                <h5><a href="">Action comics</a></h5>
+            </div>
+            <div class="comic">
+                <h5><a href="">Action comics</a></h5>
+            </div>
+            <div class="comic">
+                <h5><a href="">Action comics</a></h5>
+            </div>
+            <div class="comic">
+                <h5><a href="">Action comics</a></h5>
+            </div>
+        </div>
 
-    </div>
-
-    <nav id="home_nav">
-        <ul>
-            <li><a href="#">DIGITAL COMICS</a></li>
-            <li><a href="#">DC MERCHANDISE</a></li>
-            <li><a href="#">SUBSCRIPTION</a></li>
-            <li><a href="#">COMIC SHOP LOCATOR</a></li>
-            <li><a href="#">DC POWER VISA</a></li>
-        </ul>
-    </nav>
+        <nav id="home_nav">
+            <ul>
+                <li><a href="#">DIGITAL COMICS</a></li>
+                <li><a href="#">DC MERCHANDISE</a></li>
+                <li><a href="#">SUBSCRIPTION</a></li>
+                <li><a href="#">COMIC SHOP LOCATOR</a></li>
+                <li><a href="#">DC POWER VISA</a></li>
+            </ul>
+        </nav>
+    </section>
+    
 @endsection
