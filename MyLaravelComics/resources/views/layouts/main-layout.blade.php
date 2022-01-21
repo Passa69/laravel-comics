22fc6279a5d1e0e046d2c28f55d7c4895c26df28
tommaso <passarotto13@gmail.com>
2022-01-21 17:44:40 +0100

    correzione css delle pagine

--- MyLaravelComics/resources/views/layouts/main-layout.blade.php
+++ MyLaravelComics/resources/views/layouts/main-layout.blade.php
@@ -6,7 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="{{ asset('js/app.js') }}"></script>
-    <title>Comics</title>
+    <title>DC Comics</title>
+
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+    </style>
 </head>
 <body>
     @include('components.header')
